2dd881d4abe8c1630f5e134d84fffd7b3227d81d
Nolan Lawson <nolan.lawson@gmail.com>
2014-05-25 12:18:15 -0700

    WIP

--- bin/build-all-plugins.sh
+++ bin/build-all-plugins.sh
@@ -6,7 +6,10 @@ UGLIFY=./node_modules/uglify-js/bin/uglifyjs
 BROWSERIFY=./node_modules/.bin/browserify
 
 for plugin in $plugins; do
-  $BROWSERIFY lib/plugins/index.js \
+  $BROWSERIFY lib/plugin.js \
+    -r ./lib/deps/migrate-browser.js:../deps/migrate \
+    -r ./lib/deps/migrate-browser.js:../adapters/../deps/migrate \
+    -r ./lib/deps/migrate.js:./lib/deps/migrate-browser.js \
     -r pouchdb-${plugin}:adapter-plugin \
     -o ./dist/pouchdb.${plugin}.js
 
